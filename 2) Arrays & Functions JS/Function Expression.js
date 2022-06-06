c99ba9294c8153278ce6dfeee2ba9958ae0bd774
pj1807 <prasamjain294@gmail.com>
2022-06-07 02:10:31 +0530

    Concept of Function Expressions

---
+++ 2) Arrays & Functions JS/Function Expression.js
@@ -0,0 +1,55 @@
+/* _____-___________Function Expression(assign functions to a var.)___________________________ */
+
+// this is a function declaration - first declare it ans then use it.
+
+function fact(n)
+{
+  var ans =1;
+  for(var i=1;i<=n;i++)
+    {
+      ans*=i;
+    }
+  return ans;
+}
+
+var factorial = fact(5);
+console.log(factorial);
+
+
+//this is function Expression -
+		//One way is "named" function:
+		//Another way is "anonymous" function:
+
+
+//Named function
+var factorial = function fact(n) 
+{
+  var ans = 1;
+  for (var i = 1; i <= n; i++) {
+    ans *= i;
+  }
+  return ans;
+};
+console.log(factorial(5));
+
+//Another way is "anonymous" function:
+//If we dont give any name to a function then they take name of var.
+// in this case function name is - factorial2() but in above case name is fact().
+
+var factorial2 = function (n) {
+  var ans = 1;
+  for (var i = 1; i <= n; i++) {
+    ans *= i;
+  }
+  return ans;
+};
+console.log(factorial2);
+
+//we can pass function as a argument's : -
+
+    function ncr(n,r,factorial)
+    {
+      return factorial(n)/factorial(r)*factorial(n-r);
+    }
+    console.log(ncr(5,2,factorial));  
+
