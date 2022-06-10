f694340a7e46cf0754fe597c2b81ccdd1a4930a6
pj1807 <prasamjain294@gmail.com>
2022-06-11 02:08:27 +0530

    nested object property

---
+++ 3) Object and Timing Events/Nested Object.js
@@ -0,0 +1,19 @@
+/* NESTED OBJECT       */
+
+// here student is outer Object and inside student - "address" is inner Object.
+
+var student = 
+    {
+      name : "prasam", 
+      rollNo: 189 , 
+      marks: 95,
+      address : {city: "guna", state: "MP", PIN: 473226}      //inner Object
+    };
+
+//access porperties of nested Object using various ways :- 
+
+      student.address.state         	  // MP
+      student.address["state"]        // MP
+      student["address"].state         // MP
+      student["address"]["state"]         // MP
+	  
