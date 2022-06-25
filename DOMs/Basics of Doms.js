ea78d07a6a8a101af742eeaf2b0892c2bf9c9c00
prasam jain <prasamjain294@gmail.com>
2022-06-25 14:14:12 +0530

    Create Basics of Doms.js

---
+++ DOMs/Basics of Doms.js
@@ -0,0 +1,31 @@
+// DOMS - Document Object model
+/*
+    their are different-different objects are present in documents
+    like -
+    
+      DOCUMENT: corresponding to HTML
+      
+            document.head
+            document.body
+            document.documentElements
+            document.location
+            
+      WINDOW:
+      
+            window.alert       //for notification
+            window.screen     //deals with heigth,width etc
+            window.location    // to get the current page URL
+            window.document       // this will access the above document
+            " NOTE : document OR window.document are same " 
+*/
+
+console.log(window.document == document);    //true
+console.log(window.location == document.location);    //true
+
+function test()
+{
+  return this;
+}
+
+console.log(test() == window);     //true
+// inside f test(), 'this' returns the window object i.e equal to window. 
