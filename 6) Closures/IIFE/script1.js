9b6293708ca88a55152a962c88a6303645ceae59
prasam jain <prasamjain294@gmail.com>
2022-07-22 16:28:32 +0530

    Update script1.js

--- 6) Closures/IIFE/script1.js
+++ 6) Closures/IIFE/script1.js
@@ -1,30 +1,30 @@
 //First we create a two JS file says - script1.js and script2.js
 // if we use globle variable in both JS like
 
-var name = "file1";
-console.log(name);
+var fileName = "file1";
+console.log(fileName);
 
 // output is -
-//     file2 :           b,coz "name" variable override by the file2
+//     file2 :           b,coz "fileName" variable override by the file2
 
-// but in both the file we craete a globle variable - name but they override each other
-// so "name" is only one reference
+// but in both the file we craete a globle variable - 'fileName' but they override each other
+// so "fileName" is only one reference
 //Now, instead of making globle, we make inside a funtion scope.
 
 function print(){
-    var name = "file1";
-console.log(name);
+    var fileName = "file1";
+console.log(fileName);
 }
 print();
-//here "name" is two references.
+//here "fileName" is two references.
 
 // Now, the output is : file1 then file2
 //but we don't use globle variable at all. we can't handle it.
 // So, we use IIFE - Immediately Invoked Function Expression : in this we don't give function as name 
 
 (function(){
-    var name = "file1";
-    console.log(name);
+    var fileName = "file1";
+    console.log(fileName);
 })();
 
 //IIFE is one time use function...we can't use two or more time
